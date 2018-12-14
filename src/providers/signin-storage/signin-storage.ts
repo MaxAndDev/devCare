8bc5aa7b96401e4b42ad62bc8ea07b3ed6ea708e
Unknown <nessenmaxi@googlemail.com>
2018-12-14 11:23:07 +0100

    delete sensless code

--- src/providers/signin-storage/signin-storage.ts
+++ src/providers/signin-storage/signin-storage.ts
@@ -2,12 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttprequestProvider } from '../httprequest/httprequest';
 
-/*
-  Generated class for the SigninStorageProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class SigninStorageProvider {
 
